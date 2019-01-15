9121614bb2069c54479b77c878d65bac48ada0c5
Josh Long <josh@joshlong.com>
2019-01-14 22:58:18 -0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Bootstrap book editing like this: 
 
 ```shell
-mkdir reactive-spring-book && cd  reactive-spring-book
-curl https://raw.githubusercontent.com/reactive-spring-book/__init__/master/start.sh  | bash 
+mkdir soteria-book && cd soteria-book
+curl https://raw.githubusercontent.com/soteria-book/__init__/master/start.sh | bash 
 ```
--- start.sh
+++ start.sh
@@ -2,25 +2,24 @@
 
 start=$( cd `dirname $0` && pwd	 )
 echo "initializing from ${start} "
-curl https://raw.githubusercontent.com/reactive-spring-book/publication/master/repositories.txt | while read l ; do
+curl https://raw.githubusercontent.com/soteria-book/publication/master/repositories.txt | while read l ; do
  
- d=$(  echo $l | cut -f5 -d\/ | cut -f1 -d\.  ) 
+ d=$( echo $l | cut -f5 -d\/ | cut -f1 -d\.  ) 
  echo "Processing $d"  
  dir_to_create=${start}/$d
  
 
  if [[ -e  $dir_to_create ]] ; then 
  	echo "WARN: ${dir_to_create} aleady exists." 
- 	# if you want this to be freshly initialized then check in your work and delete the directory 
  else  	
  	echo "initializing ${dir_to_create}"  
-	git_repo=git@github.com:reactive-spring-book/${d}.git
+	git_repo=git@github.com:soteria-book/${d}.git
  	git clone ${git_repo} ${dir_to_create}
  fi
 done 
 
 cd ${start}
 
-git clone git@github.com:joshlong/reactive-spring-book.git ${start}/book 
+git clone git@github.com:joshlong/soteria-book.git ${start}/book 
 subl ${start} 
 
