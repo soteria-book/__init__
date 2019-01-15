7bae1b25701b615678506f82f2768d6ea87b192e
Josh Long <josh@joshlong.com>
2019-01-14 23:16:13 -0800

    start

--- start.sh
+++ start.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash 
 
 start=$( cd `dirname $0` && pwd	 )
-echo "initializing from ${start} "
-curl https://raw.githubusercontent.com/soteria-book/publication/master/repositories.txt | while read l ; do
- 
- d=$( echo $l | cut -f5 -d\/ | cut -f1 -d\.  ) 
- echo "Processing $d"  
- dir_to_create=${start}/$d
- 
 
- if [[ -e  $dir_to_create ]] ; then 
- 	echo "WARN: ${dir_to_create} aleady exists." 
- else  	
- 	echo "initializing ${dir_to_create}"  
-	git_repo=git@github.com:soteria-book/${d}.git
- 	git clone ${git_repo} ${dir_to_create}
- fi
+http https://raw.githubusercontent.com/soteria-book/publication/master/repositories.txt | while read l ; do
+   	
+ 	
+	if [[ -e  $dir_to_create ]] ; then 
+		echo "WARN: ${dir_to_create} aleady exists." 
+	else  	
+		echo "initializing ${dir_to_create}"  
+		git_repo=git@github.com:soteria-book/${d}.git
+		git clone ${git_repo} ${dir_to_create}
+	fi
 done 
 
 cd ${start}
